70eca763eddcbc7a0211fd93831c6e87dc971c7a
Saloni Saroha <salonisaroha5@gmail.com>
2025-08-08 17:04:35 +0530

    changes updated

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,10 +1,10 @@
 import { Component } from "@angular/core";
 @Component({
-    selector:'app-profile',
-//     template : `<h1>Profile component</h1>`,
-//    styles: [`h1 { color: green; }`]
-
-templateUrl : './profile.component.html'
+    selector: 'app-profile',
+    //     template : `<h1>Profile component</h1>`,
+    //    styles: [`h1 { color: green; }`]
+    templateUrl: './profile.component.html',
+    standalone: false
 })
 
 
