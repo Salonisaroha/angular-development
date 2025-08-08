68734b0d2ab89391ee3ae25754c9449c14eb85e3
Saloni Saroha <salonisaroha5@gmail.com>
2025-08-08 07:40:33 +0530

    Save work before Angular update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,71 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'myApp';
+  // myFunction() {
+  //   const input = prompt("Enter your age");
+  //   const age = Number(input); 
+
+  //   if (!isNaN(age)) {
+  //     if (age > 18) {
+  //       console.log("You are mature");
+  //     } else {
+  //       console.log("You need workout");
+  //     }
+  //   } else {
+  //     console.log("Invalid age entered");
+  //   }
+  //   this.otherFunction()
+  // }
+  // otherFunction(){
+  //   console.log("This is valid function!");
+  // }
+
+  // name:string|number="saloni"
+  // updateName(){
+  //   this.name = "saroha"
+  //   this.name = 123
+  // }
+  // sum(a:number,b:number){
+  //   console.log(a+b);
+  // }
+  // count = 0;
+  // handleIncrement(){
+  //   this.count = this.count + 1;
+  // }
+  // handleDecrement(){
+  //   this.count = this.count-1;
+  // }
+  // reset(){
+  //   this.count = this.count+2;
+  // }
+  // handleEvent(event:Event){
+  //   console.log("Function called", event.type);
+   
+
+  // }
+
+  name = "";
+  displayName="";
+  email="";
+  displayEmail = "";
+  getName(event:Event){
+   this.name = (event.target as HTMLInputElement).value;
+  
+  }
+  showName(){
+     this.displayName = this.name;
+  }
+  setName(){
+    this.name = "Sam";
+  }
+getEmail(val:string){
+ this.email = val;
+ console.log(this.email)
 }
+setEmail(){
+  this.email="sami123@gmail.com";
+}
+display=true;
+}
+
+
---
+++ src/app/profile/profile.component.ts
@@ -0,0 +1,13 @@
+import { Component } from "@angular/core";
+@Component({
+    selector:'app-profile',
+//     template : `<h1>Profile component</h1>`,
+//    styles: [`h1 { color: green; }`]
+
+templateUrl : './profile.component.html'
+})
+
+
+export class ProfileComponent{
+
+}
